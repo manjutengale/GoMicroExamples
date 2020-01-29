4d153f8040f86f5770588b747a894e42e4e40005
manjutengale <manjutengale9@gmail.com>
2020-01-29 19:06:36 +0530

    Go-Micro-hello

--- client.go
+++ client.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
-
+import (
+	"context"
+	"fmt"
+	micro "github.com/micro/go-micro"
+	proto "GoMicroExamples/proto"
+	"github.com/micro/go-micro/registry"
+	rm "github.com/micro/go-plugins/registry/consul"
+	grpc "github.com/micro/go-micro/service/grpc"
+)
 func main(){
+	sd := rm.NewRegistry(
+		registry.Addrs("http://127.0.0.1:8"),
+
+
+	)
 	fmt.Println("Im client")
+	service := grpc.NewService(
+		micro.Version("one"),
+		micro.Registry(sd),
+	)
+
+	clientpackage := service.Client()
+    callserver:=proto.NewBasisService("server",clientpackage)
+
+    serverresult,err:=callserver.Hello(context.Background(), &proto.Request{Name: "Amit"})
+	if err != nil {
+		fmt.Println("this is the error", err)
+
+	} else {
+		fmt.Println(serverresult) // get result
+	}
 }
--- server.go
+++ server.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
 
-func main(){
-	fmt.Println("Im server")
+
+
+	proto "GoMicroExamples/proto"
+	micro "github.com/micro/go-micro"
+	"github.com/micro/go-micro/registry"
+
+	rm "github.com/micro/go-plugins/registry/consul"
+)
+
+
+
+type basis struct {
+}
+
+func (b *basis) Hello(ctx context.Context, req *proto.Request, rsp *proto.Response) error {
+
+
+	fmt.Println("this is the server msg recv ", req.Name)
+	result := "result from server  " + req.Name
+
+	rsp.Msg = result
+	return nil
+}
+func main() {
+	sd := rm.NewRegistry(
+		registry.Addrs("http://127.0.0.1:8"),
+	)
+
+	srv := micro.NewService(
+		micro.Name("server"),
+		micro.Registry(sd),
+		micro.RegisterInterval(time.Second*10),
+		micro.RegisterTTL(time.Second*15),
+	)
+	srv.Init()
+	proto.RegisterBasisHandler(srv.Server(), new(basis))
+	if err := srv.Run(); err != nil {
+		log.Fatal(err)
+	}
+
 }
